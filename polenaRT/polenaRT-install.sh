aeb37da612a4c46eddb8b094c9ab9e159b7befda
Florian Hofer <info@florianhofer.it>
2018-09-25 13:47:59 +0200

    never version polenaRT with rebuild based on 4.9.x

--- polenaRT/polenaRT-install.sh
+++ polenaRT/polenaRT-install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 set -e
-linux_ver="4.9.98"
+linux_ver="4.9.115"
+linux_patch="rt-93"
 balena_tag="17.06-rev1"
 balena_tag=$(echo "$balena_tag" | sed 's|+|.|g')
 
@@ -69,14 +70,18 @@ mkdir -p polena-build
 cd polena-build
 
 echo
-echo "## Downloading RT patch"
-wget https://www.kernel.org/pub/linux/kernel/projects/rt/4.4/patch-${linux_ver}-rt76.patch.xz
+echo "## Downloading Linux Kernel"
+wget https://www.kernel.org/pub/linux/kernel/v4.x/linux-${linux_ver}.tar.gz
+tar xf linux-${linux_ver}.tar.gz
 
+echo
+echo "## Downloading RT patch"
+wget https://www.kernel.org/pub/linux/kernel/projects/rt/4.9/patch-${linux_ver}-${linux_patch}.patch.xz
 
 echo
 echo "## Patching Linux Kernel"
 cd linux-${linux_ver}
-xzcat ../patch-${linux_ver}-rt76.patch.xz | patch -p1
+xzcat ../patch-${linux_ver}-${linux_patch}.patch.xz | patch -p1
 
 echo
 echo "## Compiling kernel"
@@ -87,7 +92,6 @@ cd ..
 
 echo
 echo "## Installing kernel"
-cd ..
 sudo dpkg -i linux-headers-${linux_ver}-rt*.deb linux-image-${linux_ver}-rt*.deb
 
 echo
@@ -95,7 +99,7 @@ echo "## Configuring GRUB"
 #sudo sed -i -e 's/^/#/' /etc/default/grub # comment out previous GRUB config
 sudo cp /etc/default/grub /etc/default/grub.backup
 echo '
-GRUB_DEFAULT="Advanced options for Ubuntu>Ubuntu, with Linux ${linux_ver}-RT"
+GRUB_DEFAULT="Advanced options for Ubuntu>Ubuntu, with Linux '${linux_ver}'-'${linux_patch}'"
 GRUB_HIDDEN_TIMEOUT_QUIET="true"
 GRUB_TIMEOUT="10"
 GRUB_DISTRIBUTOR="`lsb_release -i -s 2> /dev/null || echo Debian`"
@@ -103,10 +107,6 @@ GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"
 GRUB_CMDLINE_LINUX=""
 ' >> grub
 sudo mv grub /etc/default/grub
-ai
-' >> ~/.xenomai_rc
-
-echo 'source ~/.xenomai_rc' >> ~/.bashrc
 
 echo "## Installing Balena"
 url="https://github.com/resin-os/balena/releases/download/${balena_tag}/balena-${balena_tag}-${arch}.tar.gz"
