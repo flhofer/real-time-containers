9ba64b09e2b79e0b99ea78979f70e05eaebd7a4b
Florian Hofer <info@florianhofer.it>
2019-02-28 18:20:55 -0800

    prj: tests with check test suite

---
+++ prj/test/test.c
@@ -0,0 +1,23 @@
+/* 
+###############################
+# test runner main script by Florian Hofer
+# last change: 28/02/2019
+# ©2019 all rights reserved ☺
+###############################
+*/
+
+
+#include <check.h>
+//#include "testPosix/pidlist.c"
+
+START_TEST (test_pidlist_create)
+{
+
+}
+END_TEST
+
+int main(void)
+{
+	return 0;
+}
+
