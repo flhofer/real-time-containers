7221f9e253a599886a29db29e00a866ba78abcd6
Florian Hofer <info@florianhofer.it>
2019-09-05 11:27:09 +0200

    sched added docker container files to test schedstat with ubuntu

---
+++ prj/container_start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+###############################
+# Bash script by Florian Hofer
+# last change: 04/09/2019
+# ©2018 all rights reserved ☺
+###############################
+
+sudo docker build -t ubuntusched .
+sudo docker run -v "$PWD":/home --cap-add=SYS_NICE --cap-add=SYS_RESOURCE --cap-add=SYS_PTRACE --cap-add=IPC_LOCK --name stat1 -it ubuntusched
+
