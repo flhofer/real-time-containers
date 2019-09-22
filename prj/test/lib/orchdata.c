608781fe65276514a2769c9d2ae80402f91e8cd7
Florian Hofer <florian.hofer@stud-inf.unibz.it>
2019-09-22 13:15:23 -0700

    sched: added test for container findparams

--- prj/test/lib/orchdata.c
+++ prj/test/lib/orchdata.c
@@ -424,6 +424,28 @@ END_TEST
 
 /// TEST CASE -> test configuration find 
 /// EXPECTED -> verifies that all parameters are found as expected 
+START_TEST(orchdata_findparams_cont)
+{	
+	// complete match, center, beginning, end
+	static const char *sigs[] = { "test123", "command", "weep 1", "keep 4"};
+
+	node_push(&head);
+	head->psig = strdup(sigs[_i]);
+	head->contid = (_i % 2) == 1 ? strdup("a2aa8c37ce4ca2aa8c37ce4c") : strdup("d7408531a3b4d7408531a3b4");
+	int retv = node_findParams(head , contparm);
+	
+	ck_assert_int_eq(retv, 0);
+	ck_assert(head->param);
+	ck_assert(head->param->cont);
+	ck_assert(head->param->cont->contid);
+	ck_assert_str_eq(head->param->cont->contid, head->contid);
+
+	node_pop(&head);
+}
+END_TEST
+
+/// TEST CASE -> test configuration find 
+/// EXPECTED -> verifies that all parameters are found as expected 
 START_TEST(orchdata_findparams_fail)
 {	
 	// complete match, center, beginning, end
@@ -461,6 +483,7 @@ void library_orchdata (Suite * s) {
 	tcase_add_unchecked_fixture(tc2, orchdata_setup, orchdata_teardown);
 	tcase_add_checked_fixture(tc2, orchdata_tc2_setup, orchdata_tc2_teardown);
 	tcase_add_loop_test(tc2, orchdata_findparams, 0, 4);
+	tcase_add_loop_test(tc2, orchdata_findparams_cont, 0, 4);
 	tcase_add_loop_test(tc2, orchdata_findparams_fail, 0, 4);
     suite_add_tcase(s, tc2);
 
