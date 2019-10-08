fa3cf1017bdae0ccd48d54752b4cb0e90ebd04a7
Florian Hofer <florian.hofer@stud-inf.unibz.it>
2019-10-08 09:44:26 +0200

    monitor .. test configurations

--- test-monitor/extractcdata.r
+++ test-monitor/extractcdata.r
@@ -1,5 +1,6 @@
 # Set the working directory
-setwd("./container/")
+setwd("./container/pretest/")
+options("width"=200)
 
 loadData <- function(fName) {
 	#Function, load data from text file into a data frame, only min, avg and max
@@ -13,21 +14,27 @@ loadData <- function(fName) {
 		return(dat)
 	}
 
-	# Transform into table, filter and add names
-	dat = read.table(file= fName)
-	#dat = read.table(text = tFile)
-	dat <- data.frame ("RunT"=dat$V3,"Period"=dat$V4,"rStart"=dat$V7)
+	if (file.info(fName)$size > 0) {
+		# Transform into table, filter and add names
+		dat = read.table(file= fName)
+		#dat = read.table(text = tFile)
+		dat <- data.frame ("RunT"=dat$V3,"Period"=dat$V4,"rStart"=dat$V7)
+	}
+	else {
+		dat <- data.frame ("RunT"=numeric(),"Period"=numeric(),"rStart"=numeric())
+	}
 	
 	return(dat)
 }
 
 #machines <- c("C5", "BM" , "T3", "T3U")
 #types <- c("dyntick", "fixtick")
-#tests <- c("1-1", "1-2", "1-3", "1-4", "1-5", "1-6", "1-7", "1-8", "1-9", "1-10", "2-1", "2-2", "3-1", "3-2", "3-3",
-# "4-1", "4-2", "4-3", "4-4", "4-5", "4-6", "4-7", "4-8", "4-9", "4-10")
-machines <- c("BM")
-types <- c("dyntick")
-tests <- c("1-9", "1-10", "3-1", "3-2", "3-3", "4-1")
+tests <- c("1-1", "1-2", "1-3", "1-4", "1-5", "1-6", "1-7", "1-8", "1-9", "1-10", "2-1", "2-2", "3-1", "3-2", "3-3",
+ "4-1", "4-2", "4-3", "4-4", "4-5", "4-6", "4-7", "4-8", "4-9", "4-10")
+machines <- c("T3", "C5")
+#types <- c("test1", "test2", "test3", "test4", "test5", "test6")
+types <- c("test5", "test6")
+#tests <- c("1-9", "1-10", "2-2","3-3", "4-9","4-10")
 
 
 for (i in 1:length(machines)) {
@@ -67,11 +74,11 @@ for (i in 1:length(machines)) {
 				r[nrow(r)+1,] <-data.frame (nr, minMin, avgMea, avgDev, maxMax, pminMin, pavgMea, pavgDev, pmaxMax, pcount)
 			}
 			print(r)
-			
 			cat ("Peak - Peak count ", maxAll, pcountAll, "\n")
 			testPcount = testPcount + pcountAll
 		}
 		cat ("Test set overruns ", testPcount , "\n")
+		cat ("----------------------------------------\n")
 	}
 }
 
