0290fd4db5e9f070d59d0d75340c79e3f4c8e3b9
Florian Hofer <florian.hofer@stud-inf.unibz.it>
2019-10-11 13:33:46 +0200

    test plots histogram

--- test-monitor/extractcdata.r
+++ test-monitor/extractcdata.r
@@ -1,6 +1,8 @@
 # Set the working directory
 setwd("./container/pretest/")
 options("width"=200)
+library(ggplot2)
+
 
 loadData <- function(fName) {
 	#Function, load data from text file into a data frame, only min, avg and max
@@ -10,7 +12,7 @@ loadData <- function(fName) {
 
 	if (!file.exists(fName)) {
 		# break here if file does not exist
-		dat <- data.frame("RunT"=numeric(),"Period"=numeric(),"rStart"=numeric())
+		dat <- data.frame ("RunT"=numeric(),"Period"=numeric(),"rStart"=numeric(), "cDur"=numeric())
 		return(dat)
 	}
 
@@ -28,19 +30,22 @@ loadData <- function(fName) {
 }
 
 #machines <- c("C5", "BM" , "T3", "T3U")
-#types <- c("dyntick", "fixtick")
 #tests <- c("1-1", "1-2", "1-3", "1-4", "1-5", "1-6", "1-7", "1-8", "1-9", "1-10", "2-1", "2-2", "3-1", "3-2", "3-3",
 # "4-1", "4-2", "4-3", "4-4", "4-5", "4-6", "4-7", "4-8", "4-9", "4-10")
-machines <- c("T3", "T3U", "C5", "BM")
+machines <- c("C5") # "T3U", "C5", "BM"
+
 types <- c("test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8")
-#types <- c("test1")
-tests <- c("1-1", "1-2", "1-3", "1-4", "1-5", "1-6", "1-7", "1-8", "1-9", "1-10")
+tests <- c("1-1", "1-2", "1-3", "1-4", "1-5", "1-6", "1-7", "1-8", "1-9", "1-10", "2-1", "2-2", "3-1", "3-2", "3-3",
+ "4-1", "4-2", "4-3", "4-4", "4-5", "4-6", "4-7", "4-8", "4-9", "4-10")
 
 for (i in 1:length(machines)) {
-	for (j in 1:length(types)) {
 
+	for (k in 1:length(tests)) {
 		testPcount = 0;
-		for (k in 1:length(tests)) {
+		tplot <- data.frame()
+
+		for (j in 1:length(types)) {
+
 
 			# Label and directory pattern
 			cat(machines[i], "-", types[j], "-", tests[k], "\n")
@@ -51,8 +56,9 @@ for (i in 1:length(machines)) {
 			maxAll = 0
 			pcountAll = 0
 
-			r<- data.frame (matrix(ncol=11,nrow=0))
-			names(r) <- c("Container","Min", "Avg", "AvgDev", "Max", "pMin", "pAvg", "pavgDev", "pMax", "pOVPeak", "pcount")
+			r<- data.frame (matrix(ncol=13,nrow=0))
+			plot <- data.frame()
+			names(r) <- c("Min", "Mdn", "Avg", "AvgDif","AvgDev", "Max", "pMin", "pMdn", "pAvg", "pavgDev", "pMax", "pOVPeak", "pcount")
 			nr = 0
 			files <- list.files(path=dir, pattern="*.log", full.names=TRUE, recursive=FALSE)
 			for (x in files) {
@@ -61,26 +67,42 @@ for (i in 1:length(machines)) {
 				dat <- loadData(x)
 				datp <- dat[!(dat$RunT > dat$cDur*1.5),]
 
-				minMin = min(datp$RunT, na.values=FALSE)
-				avgMea = mean(datp$RunT, na.values=FALSE)
+				minMin = min(datp$RunT)
+				avgMea = mean(datp$RunT)
+				avgMed = median(datp$RunT)
 				avgDev = sqrt(var(datp$RunT))
-				maxMax = max(datp$RunT, na.values=FALSE)
-				pminMin = min(datp$Period, na.values=FALSE)
-				pavgMea = mean(datp$Period, na.values=FALSE)
+				avgDif = avgMed-avgMea;
+				maxMax = max(datp$RunT)
+				pminMin = min(datp$Period)
+				pavgMea = mean(datp$Period)
+				pavgMdn = median(datp$Period)
 				pavgDev = sqrt(var(datp$Period))
-				pmaxMax = max(datp$Period, na.values=FALSE)
-				pmaxMaxp = max(dat$Period, na.values=FALSE)
+				pmaxMax = max(datp$Period)
+				pmaxMaxp = max(dat$Period)
 				pcount = sum ( dat$Period > pavgMea*1.5)
 				maxAll = max(maxMax, maxAll)
 				pcountAll = pcountAll + pcount
 
-				r[nrow(r)+1,] <-data.frame (nr, minMin, avgMea, avgDev, maxMax, pminMin, pavgMea, pavgDev, pmaxMax, pmaxMaxp, pcount)
-
+				r[nrow(r)+1,] <-data.frame (minMin, avgMed, avgMea, avgDif, avgDev, maxMax, pminMin, pavgMdn, pavgMea, pavgDev, pmaxMax, pmaxMaxp, pcount)
+				plot <- rbind(plot, dat)
 			}
+			plot$types <- types[j]
+			tplot<-rbind(tplot,plot)
 			print(r)
 			cat ("Peak - Peak count ", maxAll, pcountAll, "\n")
 			testPcount = testPcount + pcountAll
   		}
+
+  		head(tplot)
+  		pdf(file= paste0(machines[i],"_" , tests[k],".png"), width = 10, height = 10)
+  		hist <- ggplot(tplot, aes(x=RunT, fill = types), col = rainbow(7)) + 
+  			geom_density(alpha = 0.1) +
+  			xlim (c(tplot$cDur[1]*0.95,tplot$cDur[1]*1.05)) + 
+			ylab("Count") +
+			scale_fill_viridis_d()
+  		print (hist)
+  		dev.off()
+
 		cat ("Test set overruns ", testPcount , "\n")
 		cat ("----------------------------------------\n")
 	}
--- test-monitor/test.r
+++ test-monitor/test.r
@@ -1,5 +1,8 @@
 # Set the working directory
-setwd("./container/")
+setwd("./container/pretest/")
+library(ggplot2)
+library(reshape2)
+
 datacnt = 13
 
 loadData <- function(fName) {
@@ -22,9 +25,23 @@ loadData <- function(fName) {
 	return(dat)
 }
 
-pt <-loadData("C5/dyntick/2-1/rt-app-tst-20.log")
+test1 <-loadData("T3/test1/2-1/rt-app-tst-20.log")
+test2 <-loadData("T3/test3/2-1/rt-app-tst-20.log")
 
-a = mean(pt$RunT)
-b = max (pt$Period)
-print(a)
-print(b)
+test1$test <- "test1"
+test2$test <- "test2"
+
+# and combine into your new data frame vegLengths
+vegLengths <- rbind(test1, test2)
+
+# ggplot(vegLengths, aes(x=RunT, fill = test)) + 
+#    geom_histogram(alpha = 0.5, aes(y = ..density..), position = 'identity')
+ggplot(vegLengths, aes(x=RunT, fill = test)) + geom_density(alpha = 0.2)
+
+# ggplot(datt, aes(x=counts, value, color=variable)) +
+# 	#xlim (c(0,100)) + 
+# 	ylab("Count") + 
+# 	scale_x_continuous(trans='log10') +
+# 	scale_y_continuous(trans='log10') +
+#   geom_line()
+
