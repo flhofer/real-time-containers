eca4c6365dad1f0b3c066fc65ba120c43a7288f7
Florian Hofer <florian.hofer@stud-inf.unibz.it>
2020-05-03 17:14:38 -0700

    orch : separate PID allocation functions for common use

---
+++ src/orchestrator/resmgmt.c
@@ -0,0 +1,10 @@
+/*
+ * resmgmt.c
+ *
+ *  Created on: May 3, 2020
+ *      Author: Florian Hofer
+ */
+
+
+
+
---
+++ src/orchestrator/resmgmt.h
@@ -0,0 +1,15 @@
+/*
+ * resmgmt.h
+ *
+ *  Created on: May 3, 2020
+ *      Author: Florian Hofer
+ */
+
+#ifndef RESMGMT_H_
+#define RESMGMT_H_
+
+
+
+
+
+#endif /* RESMGMT_H_ */
