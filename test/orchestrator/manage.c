9570810010c903825065e9efd74208e7a0423737
Florian Hofer <florian.hofer@stud-inf.unibz.it>
2020-06-23 17:19:30 -0700

    test : removed tests requiring privileges (comment)

--- test/orchestrator/manage.c
+++ test/orchestrator/manage.c
@@ -44,70 +44,70 @@ static void orchestrator_manage_teardown() {
 	freeContParm(contparm);
 }
 
-static void orchestrator_manage_checkread(){
-	ck_assert_int_lt(0, nhead->mon.dl_count);
-	ck_assert_int_lt(0, nhead->next->mon.dl_count);
-	ck_assert_int_lt(0, nhead->next->next->mon.dl_count);
-
-	ck_assert_int_lt(0, nhead->mon.rt_avg);
-	ck_assert_int_lt(0, nhead->next->mon.rt_avg);
-	ck_assert_int_lt(0, nhead->next->next->mon.rt_avg);
-
-	ck_assert_int_le(0, nhead->mon.rt_min);
-	ck_assert_int_le(0, nhead->next->mon.rt_min);
-	ck_assert_int_le(0, nhead->next->next->mon.rt_min);
-}
+//static void orchestrator_manage_checkread(){
+//	ck_assert_int_lt(0, nhead->mon.dl_count);
+//	ck_assert_int_lt(0, nhead->next->mon.dl_count);
+//	ck_assert_int_lt(0, nhead->next->next->mon.dl_count);
+//
+//	ck_assert_int_lt(0, nhead->mon.rt_avg);
+//	ck_assert_int_lt(0, nhead->next->mon.rt_avg);
+//	ck_assert_int_lt(0, nhead->next->next->mon.rt_avg);
+//
+//	ck_assert_int_le(0, nhead->mon.rt_min);
+//	ck_assert_int_le(0, nhead->next->mon.rt_min);
+//	ck_assert_int_le(0, nhead->next->next->mon.rt_min);
+//}
 
 /// TEST CASE -> test read of run parameters of detected PID list, debug output
 /// EXPECTED -> 3 elements show changed run-times and/or deadlines
-START_TEST(orchestrator_manage_readdata)
-{	
-	pthread_t thread1;
-	int  iret1;
-	int stat1 = 0;
-	prgset->ftrace = 0;
-
-	const char * pidsig[] = {	"chrt -r 1 taskset -c " TESTCPU " sh -c 'while [ 1 ]; do echo test1 ; done'",
-								"chrt -r 2 taskset -c " TESTCPU " sh -c 'while [ 1 ]; do echo test2 ; done'",
-								"chrt -r 3 taskset -c " TESTCPU " sh -c 'while [ 1 ]; do echo test3 ; done'",
-								NULL };
-
-	int sz_test = sizeof(pidsig)/sizeof(*pidsig)-1;
-	FILE * fd[sz_test];
-	pid_t pid[sz_test];
-
-	{
-		int i =0;
-		while (pidsig[i]) {
-			// new PID
-
-			fd[i] = popen2(pidsig[i], "r", &pid[i]);
-
-			printf("created PID %d\n", pid[i]);
-			node_push(&nhead);
-			nhead->pid = pid[i];
-			nhead->psig = strdup(pidsig[i]);
-
-			i++;
-		}
-	}
-
-	iret1 = pthread_create( &thread1, NULL, thread_manage, (void*) &stat1);
-	ck_assert_int_eq(iret1, 0);
-
-	sleep(5);
-	// set stop status
-	stat1 = -1;
-
-	for (int i=0; i< sz_test; i++)
-		pclose2(fd[i], pid[i], SIGINT); // send SIGINT = CTRL+C to watch instances
-
-	if (!iret1) // thread started successfully
-		iret1 = pthread_join(thread1, NULL); // wait until end
-
-	orchestrator_manage_checkread();
-}
-END_TEST
+//START_TEST(orchestrator_manage_readdata)
+//{
+//	pthread_t thread1;
+//	int  iret1;
+//	int stat1 = 0;
+//	prgset->ftrace = 0;
+//
+//	const char * pidsig[] = {	"chrt -r 1 taskset -c " TESTCPU " sh -c 'while [ 1 ]; do echo test1 ; done'",
+//								"chrt -r 2 taskset -c " TESTCPU " sh -c 'while [ 1 ]; do echo test2 ; done'",
+//								"chrt -r 3 taskset -c " TESTCPU " sh -c 'while [ 1 ]; do echo test3 ; done'",
+//								NULL };
+//
+//	int sz_test = sizeof(pidsig)/sizeof(*pidsig)-1;
+//	FILE * fd[sz_test];
+//	pid_t pid[sz_test];
+//
+//	{
+//		int i =0;
+//		while (pidsig[i]) {
+//			// new PID
+//
+//			fd[i] = popen2(pidsig[i], "r", &pid[i]);
+//
+//			printf("created PID %d\n", pid[i]);
+//			node_push(&nhead);
+//			nhead->pid = pid[i];
+//			nhead->psig = strdup(pidsig[i]);
+//
+//			i++;
+//		}
+//	}
+//
+//	iret1 = pthread_create( &thread1, NULL, thread_manage, (void*) &stat1);
+//	ck_assert_int_eq(iret1, 0);
+//
+//	sleep(5);
+//	// set stop status
+//	stat1 = -1;
+//
+//	for (int i=0; i< sz_test; i++)
+//		pclose2(fd[i], pid[i], SIGINT); // send SIGINT = CTRL+C to watch instances
+//
+//	if (!iret1) // thread started successfully
+//		iret1 = pthread_join(thread1, NULL); // wait until end
+//
+//	orchestrator_manage_checkread();
+//}
+//END_TEST
 
 /// TEST CASE -> Stop manage thread when setting status to -1
 /// EXPECTED -> exit after 2 seconds, no error
@@ -133,44 +133,44 @@ END_TEST
 
 /// TEST CASE -> test read of run parameters of detected PID list, fTrace read
 /// EXPECTED -> 3 elements show changed run-times and/or deadlines
-START_TEST(orchestrator_manage_readftrace)
-{
-	pthread_t thread1;
-	int  iret1;
-	struct ftrace_thread fthread;
-	fthread.dbgfile = malloc(MAX_PATH); // it's freed inside the thread
-	fthread.cpuno = 1; // dummy value
-	(void)sprintf(fthread.dbgfile, "test/manage_ftread.dat"); // dump of a kernel thread scan
-
-	prgset->ftrace = 1;
-
-
-	const int pid[] = { 1, 2, 3	}; // to do setup
-
-	for (int i=0; i<sizeof(pid)/sizeof(int); ++i) {
-		node_push(&nhead);
-		nhead->pid = pid[i];
-		nhead->psig = strdup("");
-	}
-
-	buildEventConf();
-
-	iret1 = pthread_create( &thread1, NULL, thread_ftrace, (void*)&fthread);
-	ck_assert_int_eq(iret1, 0);
-
-	sleep(2);
-
-	// tell fTrace threads to stop
-	(void)pthread_kill (thread1, SIGQUIT);
-
-	if (!iret1) // thread started successfully
-		iret1 = pthread_join( thread1, NULL); // wait until end
-
-	clearEventConf();
-
-	orchestrator_manage_checkread();
-}
-END_TEST
+//START_TEST(orchestrator_manage_readftrace)
+//{
+//	pthread_t thread1;
+//	int  iret1;
+//	struct ftrace_thread fthread;
+//	fthread.dbgfile = malloc(MAX_PATH); // it's freed inside the thread
+//	fthread.cpuno = 1; // dummy value
+//	(void)sprintf(fthread.dbgfile, "test/manage_ftread.dat"); // dump of a kernel thread scan
+//
+//	prgset->ftrace = 1;
+//
+//
+//	const int pid[] = { 1, 2, 3	}; // to do setup
+//
+//	for (int i=0; i<sizeof(pid)/sizeof(int); ++i) {
+//		node_push(&nhead);
+//		nhead->pid = pid[i];
+//		nhead->psig = strdup("");
+//	}
+//
+//	buildEventConf();
+//
+//	iret1 = pthread_create( &thread1, NULL, thread_ftrace, (void*)&fthread);
+//	ck_assert_int_eq(iret1, 0);
+//
+//	sleep(2);
+//
+//	// tell fTrace threads to stop
+//	(void)pthread_kill (thread1, SIGQUIT);
+//
+//	if (!iret1) // thread started successfully
+//		iret1 = pthread_join( thread1, NULL); // wait until end
+//
+//	clearEventConf();
+//
+//	orchestrator_manage_checkread();
+//}
+//END_TEST
 
 void orchestrator_manage (Suite * s) {
 	TCase *tc1 = tcase_create("manage_thread_stop");
@@ -180,12 +180,14 @@ void orchestrator_manage (Suite * s) {
 	tcase_set_timeout(tc1, 10);
 	suite_add_tcase(s, tc1);
 
+	/* these depend on privileges and can not be run in the cloud
 	TCase *tc2 = tcase_create("manage_thread_read");
 	tcase_add_checked_fixture(tc2, orchestrator_manage_setup, orchestrator_manage_teardown);
 	tcase_add_test(tc2, orchestrator_manage_readdata);
 	tcase_add_test(tc2, orchestrator_manage_readftrace);
 	tcase_set_timeout(tc2, 10);
     suite_add_tcase(s, tc2);
+	*/
 
 	return;
 }
