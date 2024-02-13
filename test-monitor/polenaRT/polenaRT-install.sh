6b15d4917c63eec8fd69c8d65d2bdb6f6af80fe4
Florian Hofer <info@florianhofer.it>
2024-02-13 14:56:14 +0100

    polenaRT installer review version and compiler fetch - new kernels

--- test-monitor/polenaRT/polenaRT-install.sh
+++ test-monitor/polenaRT/polenaRT-install.sh
@@ -1,55 +1,71 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
-linux_ver="4.19.50"
-linux_patch="rt22"
-linux_root=$(echo "$linux_ver" | sed -n 's/\([0-9]*\.[0-9]*\).*/\1/p')
-#balena_tag="17.06-rev1"
-#balena_tag=$(echo "$balena_tag" | sed 's|+|.|g')
+
+# Long term Kernel Suggestions
+#   - 22.04 LTS -> 6.1.77 - rt24
+#   - newest LT 6.6.15 - rt22
+# Latest Stable  6.7 - rt6 ( 6.7.4? )
+
+linux_patch=${1:-'6.1.77-rt24'}
+balena_tag=${2:-'v20.10.19'}
+
+rt_patch=$(echo "$linux_patch" | sed -n 's/[0-9.]*-\(.*\)/\1/p')
+linux_ver=$(echo "$linux_patch" | sed -n 's/\([0-9]*\(\.[0-9]*\)\{1,2\}\).*/\1/p')
+linux_root=$(echo "$linux_ver" | sed -n 's/\([0-9]*\).*/\1/p')
+linux_base=$(echo "$linux_ver" | sed -n 's/\([0-9]*\.[0-9]*\).*/\1/p')
+balena_rev=$(echo "$balena_tag" | sed 's|+|.|g')
+
+# Check and warn about missing required commands before doing any actual work.
+abort=0
+for cmd in wget curl tar; do
+	if [ -z "$(command -v $cmd)" ]; then
+		cat >&2 <<-EOF
+		Error: unable to find required command: $cmd
+		EOF
+		abort=1
+	fi
+done
+[ $abort = 1 ] && exit 1
+
+sudo=
+if [ "$(id -u)" -ne 0 ]; then
+	if [ -z "$(command -v sudo)" ]; then
+		cat >&2 <<-EOF
+		Error: this installer needs the ability to run commands as root.
+		You are not running as root and we are unable to find "sudo" available.
+		EOF
+		exit 1
+	fi
+	sudo="sudo -E"
+fi
+
+#echo "Selecting RT-patch ${rt_patch} for kernel root family ${linux_root}, base version ${linux_base}, release ${linux_ver}"
 
 machine=$(uname -m)
 
+# Detect the system architecture
 case "$machine" in
 	"armv5"*)
-		arch="armv5"
-		echo "$arch not supported yet."
-		exit 1
+		arch="armv5e"
 		;;
 	"armv6"*)
-		arch="armv6"
-		echo "$machine not supported yet."
-		exit 1
+		arch="armv6l"
 		;;
 	"armv7"*)
-		arch="armv7"
-		echo "$arch not supported yet."
-		exit 1
+		arch="armv7hf"
 		;;
 	"armv8"*)
-		arch="aarch64"
-		echo "$arch not supported yet."
-		exit 1
+		arch="arm64"
 		;;
 	"aarch64"*)
-		arch="aarch64"
-		echo "$arch not supported yet."
-		exit 1
-		;;
-	"i386")
-		arch="i386"
-		echo "$arch not supported yet."
-		exit 1
-		;;
-	"i686")
-		arch="i386"
-		echo "$arch not supported yet."
-		exit 1
+		arch="arm64"
 		;;
 	"x86_64")
-		arch="x86_64"
+		arch="amd64"
 		;;
 	*)
-		echo "Unknown machine type: $machine"
+		echo "Unknown machine type: $machine" >&2
 		exit 1
 esac
 
@@ -66,7 +82,7 @@ sudo apt-get install -y qt5-default
 sudo apt-get install -y checkinstall
 
 ################################
-# Xenomai
+# Preempt RT
 ################################
 mkdir -p polena-build
 cd polena-build
@@ -74,7 +90,7 @@ cd polena-build
 if [ ! -e "./linux-${linux_ver}" ]; then
 	echo
 	echo "## Downloading Linux Kernel"
-	wget https://www.kernel.org/pub/linux/kernel/v4.x/linux-${linux_ver}.tar.gz
+	wget https://www.kernel.org/pub/linux/kernel/v${linux_root}.x/linux-${linux_ver}.tar.gz
 else
 	# cleanup
 	rm -r linux-${linux_ver}
@@ -84,32 +100,32 @@ tar xf linux-${linux_ver}.tar.gz
 if [ ! -f "patch-${linux_ver}-${linux_patch}.patch.xz" ]; then
 	echo
 	echo "## Downloading RT patch"
-	wget https://www.kernel.org/pub/linux/kernel/projects/rt/${linux_root}/patch-${linux_ver}-${linux_patch}.patch.xz
+	wget https://www.kernel.org/pub/linux/kernel/projects/rt/${linux_base}/patch-${linux_patch}.patch.xz
 
 fi
 
 echo
 echo "## Patching Linux Kernel"
 cd linux-${linux_ver}
-xzcat ../patch-${linux_ver}-${linux_patch}.patch.xz | patch -p1
+xzcat ../patch-${linux_patch}.patch.xz | patch -p1
 
 echo
 echo "## Compiling kernel"
-cp ../../ubuntu-x86_64.config .config
+cp ../../ubuntu-x86_64-${linux_patch}.config .config
 yes "" | make oldconfig
 CONCURRENCY_LEVEL=$(nproc) make-kpkg --rootcmd fakeroot --initrd kernel_image kernel_headers
 cd ..
 
 echo
 echo "## Installing kernel"
-sudo dpkg -i linux-headers-${linux_ver}-rt*.deb linux-image-${linux_ver}-rt*.deb
+sudo dpkg -i linux-headers-${linux_patch}.deb linux-image-${linux_patch}.deb
 
 echo
 echo "## Configuring GRUB"
 #sudo sed -i -e 's/^/#/' /etc/default/grub # comment out previous GRUB config
 sudo cp /etc/default/grub /etc/default/grub.backup
 echo '
-GRUB_DEFAULT="Advanced options for Ubuntu>Ubuntu, with Linux '${linux_ver}'-'${linux_patch}'"
+GRUB_DEFAULT="Advanced options for Ubuntu>Ubuntu, with Linux '${linux_patch}'"
 GRUB_HIDDEN_TIMEOUT_QUIET="true"
 GRUB_TIMEOUT="10"
 GRUB_DISTRIBUTOR="`lsb_release -i -s 2> /dev/null || echo Debian`"
@@ -120,7 +136,7 @@ sudo mv grub /etc/default/grub
 sudo update-grub2
 
 #echo "## Installing Balena"
-#url="https://github.com/resin-os/balena/releases/download/${balena_tag}/balena-${balena_tag}-${arch}.tar.gz"
+#url="https://github.com/resin-os/balena/releases/download/${balena_rev}/balena-${balena_rev}-${arch}.tar.gz"
 #curl -sL "$url" | tar xzv -C /usr/local/bin --strip-components=1
 
 cat <<EOF
