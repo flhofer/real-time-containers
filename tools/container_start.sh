c19cf23a6d27d785077383495626669031e4f79c
Florian Hofer <info@florianhofer.it>
2024-03-13 09:45:04 +0100

    tools add alpine dockerfile for run
    renamed ubuntu file with extension

--- tools/container_start.sh
+++ tools/container_start.sh
@@ -5,6 +5,6 @@
 # ©2020 all rights reserved ☺
 ###############################
 
-sudo docker build -t ubuntusched .
+sudo docker build -f Dockerfile-ubuntu -t ubuntusched .
 sudo docker run --rm -v "$PWD/../":/home -v /sys/fs/cgroup/:/sys/fs/cgroup/ --cap-add=SYS_NICE --cap-add=SYS_RESOURCE --cap-add=SYS_PTRACE --cap-add=IPC_LOCK --name stat1 -it ubuntusched
 
