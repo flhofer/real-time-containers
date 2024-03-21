7da03834a26099f36a7dfc3e6825899c9f0ff30e
Florian Hofer <info@florianhofer.it>
2024-03-21 10:08:01 +0100

    polenaRT config add cpu/numa detection

--- test-monitor/polenaRT/polenaRT-config.sh
+++ test-monitor/polenaRT/polenaRT-config.sh
@@ -21,6 +21,25 @@ EOF
 echo "*** WIP *** -- Not completed yet"
 exit 1
 
+#get number of cpu-threads
+prcs=$(nproc --all)
+
+#get number of numa nodes
+numanr=$(lscpu | grep NUMA | grep 'node(s)' -m 1 | awk '{print $3}')
+
+if [ $numanr -ge 2 ]; then
+
+	#get cpus assigned to numa nodes / split there for better performance
+	for ((i=0;i<$numanr;i++)); do 
+		numa[$i]=$(lscpu | grep NUMA | grep 'node'$i'' -m 1 | awk '{print $4}')
+	done
+
+else
+	echo "Single NUMA node detected, selecting all excepet cpu0 for isolation.."
+	numa[0]='1-'$((prcs-1)) # string
+fi	
+
+
 function irq_affinity() {
 	echo "Setting IRQ affinity to "$1
 	for file in /proc/irq/*/; do
@@ -53,7 +72,7 @@ function restartCores () {
 		#simple number of cores
 
 		#shut down cores
-		for ((i=1;i<$prcs;i++))
+		for ((i=$prcs-1;i>0;i--))
 		do 
 			echo "Setting CPU"$i" offline..."
 			$(echo 0 > /sys/devices/system/cpu/cpu$i/online)
