481118183c758883e4b6755026b1656cc035b03e
Florian Hofer <info@florianhofer.it>
2024-04-03 08:49:47 +0200

    polenaRT config smt switch

--- test-monitor/polenaRT/polenaRT-config.sh
+++ test-monitor/polenaRT/polenaRT-config.sh
@@ -19,7 +19,8 @@ simply real-time containers
 EOF
 
 echo "*** WIP *** -- Not completed yet"
-exit 1
+
+syscpu="/sys/devices/system/cpu"
 
 #get number of cpu-threads
 prcs=$(nproc --all)
@@ -27,20 +28,7 @@ prcs=$(nproc --all)
 #get number of numa nodes
 numanr=$(lscpu | grep NUMA | grep 'node(s)' -m 1 | awk '{print $3}')
 
-if [ $numanr -ge 2 ]; then
-
-	#get cpus assigned to numa nodes / split there for better performance
-	for ((i=0;i<$numanr;i++)); do 
-		numa[$i]=$(lscpu | grep NUMA | grep 'node'$i'' -m 1 | awk '{print $4}')
-	done
-
-else
-	echo "Single NUMA node detected, selecting all excepet cpu0 for isolation.."
-	numa[0]='1-'$((prcs-1)) # string
-fi	
-
-
-function irq_affinity() {
+irq_affinity() {
 	echo "Setting IRQ affinity to "$1
 	for file in /proc/irq/*/; do
 	   echo $1 > $file/smp_affinity;
@@ -48,14 +36,32 @@ function irq_affinity() {
 	echo $1 > /proc/irq/default_smp_affinity
 	}
 
-function load_balancer() {
+smt_switch () {
+	################################
+	# control SMT switch
+	################################
+	local switch=$1
+
+	case "$switch" in
+	
+		on|off )
+			$sudo sh -c "echo $switch > ${syscpu}/smt/control"
+			;;
+			
+		*)
+			echo "ERROR: parameter ${switch} invalid for SMT"
+			;;
+		esac
+}
+
+load_balancer() {
 	echo "Setting load balancer to "$1
 	echo $1 > /sys/fs/cgroup/cpuset/cpuset.sched_load_balance
 	echo $1 > /sys/fs/cgroup/cpuset/user/cpuset.sched_load_balance
 	echo 1 > /sys/fs/cgroup/cpuset/system/cpuset.sched_load_balance
 	}
 
-function restartCores () {
+restartCores () {
 	# verify if core 0 is disableable
 	if [ -e "/sys/devices/system/cpu/cpu0/online" ]; then
 		echo "Setting CPU0 offline..."
@@ -66,13 +72,28 @@ function restartCores () {
 		$(echo 1 > /sys/devices/system/cpu/cpu0/online)
 	fi
 
+	if false; then
+	if [ $numanr -ge 2 ]; then
+
+		#get cpus assigned to numa nodes / split there for better performance
+		for i in 0..$numanr; do 
+			numa[$i]=$(lscpu | grep NUMA | grep 'node'$i'' -m 1 | awk '{print $4}')
+		done
+
+	else
+		echo "Single NUMA node detected, selecting all excepet cpu0 for isolation.."
+		numa[0]='1-'$((prcs-1)) # string
+	fi	
+	fi
+
 	#check if numa is a number or a list
 	re='^[0-9]+$';
 	if [[ $numanr -eq 1 ]]; then
 		#simple number of cores
 
 		#shut down cores
-		for ((i=$prcs-1;i>0;i--))
+		local start=$(($prcs-1))
+		for i in `seq $start -1 1` 
 		do 
 			echo "Setting CPU"$i" offline..."
 			$(echo 0 > /sys/devices/system/cpu/cpu$i/online)
@@ -82,7 +103,8 @@ function restartCores () {
 		sleep 1
 
 		# put them back online
-		for ((i=1;i<$prcs;i++))
+		local end=$start
+		for i in `seq 1 $end`
 		do
 			echo "Putting CPU"$i" back online..."
 			$(echo 1 > /sys/devices/system/cpu/cpu$i/online)
@@ -115,4 +137,18 @@ function restartCores () {
 	fi
 }
 
+############### Check privileges ######################
+sudo=
+if [ "$(id -u)" -ne 0 ]; then
+	if [ -z "$(command -v sudo)" ]; then
+		cat >&2 <<-EOF
+		Error: this installer needs the ability to run commands as root.
+		You are not running as root and we are unable to find "sudo" available.
+		EOF
+		exit 1
+	fi
+	sudo="sudo -E"
+fi
+
+smt_switch on
 
