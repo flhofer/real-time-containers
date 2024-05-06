da69e1520e91eb94499252f458496399eecb647b
Florian Hofer <info@florianhofer.it>
2024-05-06 20:10:22 +0200

    tools: add kernel reboot-test script

---
+++ tools/kernel-reboot.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+if [[ ! "$1" == "quiet" ]]; then
+
+cat <<EOF
+
+######################################
+
+Kernel test execution
+______     _
+| ___ \   | |
+| |_/ /__ | | ___ _ __   __ _ 
+|  __/ _ \| |/ _ \ '_ \ / _' |
+| | | (_) | |  __/ | | | (_| |
+\_|  \___/|_|\___|_| |_|\__,_|
+
+simply real-time containers
+
+######################################
+
+EOF
+else 
+	shift
+fi
+
+function print_help {
+
+	cat <<-EOF
+	Usage: $0 start [kernel-ver1] [kernel-ver2]
+	       $0 list
+
+	for a test-reboot to a specified kernel with timeout in cron
+	
+	where:
+	kernel-ver1      The kernel to test booting
+	kernel-ver2      The kernel we want to return to in case of failure, defaults to running
+	EOF
+	
+}
+
+cmd=${1:-'help'}
+
+if [ $# -gt 3 ]; then
+	echo "Too many arguments supplied!!"
+	print_help
+	exit 1
+fi
+if [ "$cmd" = "help" ]; then
+	print_help
+	exit 0
+fi
+
+############### Check privileges ######################
+sudo=
+if [ "$(id -u)" -ne 0 ]; then
+	if [ -z "$(command -v sudo)" ]; then
+		cat >&2 <<-EOF
+		Error: this script needs the ability to run commands as root.
+		You are not running as root and we are unable to find "sudo" available.
+		EOF
+		exit 1
+	fi
+	sudo="sudo -E"
+fi
+
+# find penultimate kernel config installed, use as default for reboot version
+# else, it must be one of the installed, so break
+verold=
+for ver in /boot/config-* ; do
+	ver=${ver#/boot/config-}
+	if [ "$ver" = "$2" ]; then
+		ver1=$2
+		break;
+	fi
+	ver1=$verold
+	verold=$ver
+done
+
+[ -n "$2" ] && [ ! "$2" = "$ver1" ] && echo "Error: Kernel version '$2' specified not found !" && exit 1;
+
+ver2=${3:-$(uname -r)}
+
+verold=
+abort=1
+for ver in /boot/config-* ; do
+	ver=${ver#/boot/config-}
+	if [ "$ver" = "$ver2" ]; then
+		abort=0
+		break;
+	fi
+done
+[ $abort = 1 ] && echo "Error: Kernel version '$3' specified not found !" && exit 1;
+
+##################### Update grub and restart ##########################
+
+if [ "$cmd" = "list" ]; then
+
+	echo "Kernel versions available to select from"
+	for ver in /boot/config-* ; do
+		ver=${ver#/boot/config-}
+		echo "* $ver"
+	done
+
+elif [ "$cmd" = "start" ]; then
+
+	# set new bootkernel
+	$sudo sh -c "sed -i '/#GRUB_DEFAULT=/s/#GRUB_DEFAULT=/GRUB_DEFAULT=/' /etc/default/grub"
+	$sudo sh -c "sed -i '/GRUB_DEFAULT/s/Linux.*\"/Linux ${ver1}\"/' /etc/default/grub"
+
+	# minutes now
+	min=$(date +'%M')
+	# add to cron a timeout 1 hr, restart with reset
+	$sudo sh -c "echo '${min} */1 * * * cd "${PWD}" && ${0} reset ${ver2} ' | crontab -u root -"
+
+	$sudo update-grub
+
+	$sudo reboot
+
+elif [ "$cmd" = "reset" ]; then
+	# on reset first ver is ver to use
+	eval $sudo sh -c "sed -i '/GRUB_DEFAULT/s/Linux.*\"/Linux ${ver1}\"/' /etc/default/grub"
+
+	# remove start script
+	$sudo crontab -u root -r
+
+	$sudo update-grub
+
+	$sudo reboot
+else
+	echo "Unknown command '$1'"
+	print_help
+	exit 1
+fi
+
