18ef3c0bb5929a40e63c673903ed3b010b434c27
Florian Hofer <info@florianhofer.it>
2024-06-09 10:17:12 +0200

    uc3: vplc_cont cleanup argument order

--- test-monitor/usecase/uc3/vplc_cont.sh
+++ test-monitor/usecase/uc3/vplc_cont.sh
@@ -18,11 +18,9 @@ simply real-time containers
 
 EOF
 
-card=${2:-"eth0"}
-name=${3:-"runtime"}
+name=${2:-"runtime"}
 if [ "$1" = "start" ]; then
-  card=${2:-"eth0"}
-  name=${3:-"runtime"}
+  card=${3:-"eth0"}
   affin=
   if [ -n "$4" ]; then
   	affin="--cpuset-cpus=${4}"
@@ -30,7 +28,6 @@ if [ "$1" = "start" ]; then
   # run codesys vControl with additional TMP mapping for log output
   docker run --rm -td -v /tmp:/tmp -v /var/opt/codesysvcontrol/instances/${name}/conf/codesyscontrol:/conf/codesyscontrol/ -v /var/opt/codesysvcontrol/instances/${name}/data/codesyscontrol:/data/codesyscontrol/ --cap-add=IPC_LOCK --cap-add=NET_ADMIN --cap-add=NET_BROADCAST --cap-add=SETFCAP --cap-add=SYS_ADMIN --cap-add=SYS_MODULE --cap-add=SYS_NICE --cap-add=SYS_PTRACE --cap-add=SYS_RAWIO --cap-add=SYS_RESOURCE --cap-add=SYS_TIME ${affin} --hostname ${name} --name ${name} codesyscontrol_virtuallinux:4.11.0.0-b.trunk.170 -n ${card}
 else
-  name=${2:-"runtime"}
   docker stop ${name}
 fi
 
