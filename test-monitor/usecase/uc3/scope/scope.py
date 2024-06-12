3e43aa3fd0916f4a09935f7d15bfe62af1ada1a8
Florian Hofer <info@florianhofer.it>
2024-06-12 15:05:58 +0200

    uc3: scope tests and setup - first func

--- test-monitor/usecase/uc3/scope/scope.py
+++ test-monitor/usecase/uc3/scope/scope.py
@@ -12,7 +12,7 @@ Created on Jun 12, 2024
 '''
 
 import vxi11
-from sympy.codegen.ast import none
+import re
 
 class Scope(object):
     '''
@@ -28,18 +28,81 @@ class Scope(object):
             self._instr =  vxi11.Instrument(ip_addr)
             self._instr.timeout = 1000
             print("Connected to :  ", self._instr.ask("*IDN?"))
+            print("Status : ", self._instr.ask("ALST?"))
         except Exception as e:
             raise (e) 
         
         
     def setScreen(self):
+        '''
+        Set screen and channel values to match our display area
+        24V pulsing singal at ~1KHz
+        '''
         
-        print(self._instr.ask("ALST?"))
+        self._instr.ask("C2:TRA ON")    # enable channel 2
+
+        self._instr.ask("C1:VDIV 3.5V") # 8 Div's total = 28Vpp
+        self._instr.ask("C2:VDIV 3.5V") # 8 Div's total = 28Vpp
+
+        self._instr.ask("TDIV 100us")   # Time division hor, 16 divs= 1,6 ms
         
-    def setChannels(self):
-        pass
+        self._instr.ask("C1:OFST -12V") # Offset Ver
+        self._instr.ask("C2:OFST -12V") # Offset Ver
         
+        self._instr.ask("TDIV 50us")    # set to 50us hor div
+        self._instr.ask("TRDL 350us")   # set h offset to 350 to allow right slack..
+
+        self._instr.ask("C1:TRSL POS")  # Positive trigger
+        self._instr.ask("C2:TRSL POS")  # Positive trigger
+
+        self._instr.ask("SET50")        # Set trigger level to 50% pp 
+        
+        self._instr.ask("C1:ATTN 1")    # Set probe attenuation to 1x
+        self._instr.ask("C2:ATTN 1")    # Set probe attenuation to 1x        
+    
+    def checkSampleRate(self):
+        '''
+        Verify if the sample rate is high enough to find glitches
+        '''
+        
+        rateStr=self._instr.ask("SARA?").removeprefix("SARA ")
+        prs = re.compile('([0-9.]+)\s*(\w+)')
+        rateBase, rateSuff = prs.match(rateStr).groups()
+        rate = float(rateBase)
+        if 'G' == rateSuff[0]:
+            rate *= 1000000
+        if 'M' == rateSuff[0]:
+            rate *= 1000000
+        if 'K' == rateSuff[0]:
+            rate *= 1000
+        
+        freqStr = self._instr.ask("CYMT?")
+        freqBase, freqSuff = prs.match(freqStr).groups()
+        freq = float(freqBase)
+        if 'G' == freqSuff[0]:
+            freq *= 1000000
+        if 'M' == freqSuff[0]:
+            freq *= 1000000
+        if 'K' == freqSuff[0]:
+            freq *= 1000
+        
+        if rate > 5000 * freq:
+            raise 
+                
+    def setCursors(self):
+        '''
+        Set cursors and/or measurements to perform on the input signal
+        '''
+        
+        self._instr.ask("MEAD FRR,C1-C2")   # set delay measurement 
+
         
     def measureJitter(self):
-
+        ''' 
+        Ask the instrument to measure the delay between channels
+        -> use FRR = difference time first rising edge to first rising edge
+        '''
+        del= self._instr.ask("MEAD FRR,C1-C2")
+        
         return 0
+    
