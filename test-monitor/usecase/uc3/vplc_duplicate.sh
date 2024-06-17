e86ba9857d2377aef7341504d87ea0ab9ea1b78a
Florian Hofer <info@florianhofer.it>
2024-06-17 10:53:43 +0200

    uc3: vplc duplicator / instance copy and ram-drive allocation script

---
+++ test-monitor/usecase/uc3/vplc_duplicate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+VPLCDIR="/var/opt/codesysvcontrol/instances/"
+
+rm -r /tmp/codesyscontrol*
+
+for i in {1..10}; do
+  rm -r ${VPLCDIR}runtime${i}
+  cp -r ${VPLCDIR}runtime/ ${VPLCDIR}runtime${i}/
+  cp -r ${VPLCDIR}runtime/data/codesyscontrol/ /tmp/codesyscontrol${i} 
+  rm -r ${VPLCDIR}runtime${i}/data/codesyscontrol
+  ln -s /tmp/codesyscontrol${i} ${VPLCDIR}runtime${i}/data/codesyscontrol
+done
+
