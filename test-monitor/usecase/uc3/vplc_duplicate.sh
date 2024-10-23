db993dd979554c657b5f7fdbd3cb8e62df076f63
Florian Hofer <info@florianhofer.it>
2024-10-23 12:47:31 +0200

    uc3 change duplicate container to allow for other origin containers

--- test-monitor/usecase/uc3/vplc_duplicate.sh
+++ test-monitor/usecase/uc3/vplc_duplicate.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
 
+if [ ! "$1" = "quiet" ]; then
+	cat <<-EOF
+
+	######################################
+
+	Test container duplication
+	______     _
+	| ___ \   | |
+	| |_/ /__ | | ___ _ __   __ _ 
+	|  __/ _ \| |/ _ \ '_ \ / _' |
+	| | | (_) | |  __/ | | | (_| |
+	\_|  \___/|_|\___|_| |_|\__,_|
+
+	simply real-time containers
+
+	######################################
+
+	EOF
+else 
+	shift
+fi
+
+#select origin runtime container
+runtime=${1:"runtime"}
+
 VPLCDIR="/var/opt/codesysvcontrol/instances/"
 
 rm -r /tmp/codesyscontrol*
 
-rm ${VPLCDIR}runtime/data/codesyscontrol/*.log
+rm ${VPLCDIR}${runtime}/data/codesyscontrol/*.log
 
 for i in {1..10}; do
 	rm -r ${VPLCDIR}runtime${i}
 	if [ -z "$1" ]; then
-	  cp -r ${VPLCDIR}runtime/ ${VPLCDIR}runtime${i}/
-	  cp -r ${VPLCDIR}runtime/data/codesyscontrol/ /tmp/codesyscontrol${i} 
+	  cp -r ${VPLCDIR}${runtime}/ ${VPLCDIR}runtime${i}/
+	  cp -r ${VPLCDIR}${runtime}/data/codesyscontrol/ /tmp/codesyscontrol${i} 
 	  rm -r ${VPLCDIR}runtime${i}/data/codesyscontrol
 	  ln -s /tmp/codesyscontrol${i} ${VPLCDIR}runtime${i}/data/codesyscontrol
 	fi
