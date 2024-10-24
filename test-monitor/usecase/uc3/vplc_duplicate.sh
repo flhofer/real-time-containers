487e94da4ef68140c6d0b52bab592fe52f0755c5
Florian Hofer <info@florianhofer.it>
2024-10-24 12:44:08 +0200

    uc3 duplicate script update container config auto

--- test-monitor/usecase/uc3/vplc_duplicate.sh
+++ test-monitor/usecase/uc3/vplc_duplicate.sh
@@ -34,10 +34,14 @@ rm ${VPLCDIR}${runtime}/data/codesyscontrol/*.log
 for i in {1..10}; do
 	rm -r ${VPLCDIR}runtime${i}
 	if [ -z "$2" ]; then
-	  cp -r ${VPLCDIR}${runtime}/ ${VPLCDIR}runtime${i}/
+	  mkdir -p ${VPLCDIR}runtime${i}/
+	  mkdir -p ${VPLCDIR}runtime${i}/data/
+	  cp ${VPLCDIR}${runtime}/VirtualInstance.cfg ${VPLCDIR}runtime${i}/
 	  cp -r ${VPLCDIR}${runtime}/data/codesyscontrol/ /tmp/codesyscontrol${i} 
-	  rm -r ${VPLCDIR}runtime${i}/data/codesyscontrol
+	  ln -s ${VPLCDIR}${runtime}/conf/ ${VPLCDIR}runtime${i}/conf
 	  ln -s /tmp/codesyscontrol${i} ${VPLCDIR}runtime${i}/data/codesyscontrol
+	  sed -i -e '/var/s/\/'${runtime}'\//\/runtime'${i}'\//g' ${VPLCDIR}runtime${i}/VirtualInstance.cfg 
+	  sed -i -e '/Hostname/s/'${runtime}'/runtime'${i}'/' ${VPLCDIR}runtime${i}/VirtualInstance.cfg 
 	fi
 done
 
