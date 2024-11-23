d4472d26f2a59c89629febeee95d276b54aa97bb
Florian Hofer <info@florianhofer.it>
2024-11-23 16:10:29 +0100

    orch test add checkperiod test for new values

--- test/orchestrator/resmgntTest.c
+++ test/orchestrator/resmgntTest.c
@@ -190,7 +190,7 @@ START_TEST(checkPeriodTest)
 	push((void**)&rHead, sizeof(struct resTracer));
 	rHead->basePeriod = 70000;
 	rHead->affinity = numa_allocate_cpumask();
-	numa_bitmask_setbit(rHead->affinity, 1);
+	numa_bitmask_setbit(rHead->affinity, 2);
 	rHead->status = MSK_STATHRMC;
 
 	struct sched_attr par ={
@@ -214,7 +214,14 @@ START_TEST(checkPeriodTest)
 	ck_assert_ptr_eq(checkPeriod(&par, -99, -1), rHead->next);		// no perfect fit, prefer better U fit
 	rHead->basePeriod = 100000;
 	par.sched_period = 100000;
-	ck_assert_ptr_eq(checkPeriod(&par, -1, -1), rHead);				// par, prefer affinity
+	rHead->next->next->U = 0.1;
+	ck_assert_ptr_eq(checkPeriod(&par, -2, -1), rHead);				// par CPU 0 and 2, prefer affinity
+	rHead->next->U = 0.1;
+	rHead->next->basePeriod = 100000;
+	ck_assert_ptr_eq(checkPeriod(&par, -2, -1), rHead);				// all par, prefer affinity
+	ck_assert_ptr_eq(checkPeriod(&par, -2, 1), rHead);				// all par, prefer affinity
+	ck_assert_ptr_eq(checkPeriod(&par, -99, 1), rHead->next);		// then running CPU
+	rHead->next->basePeriod = 50000;
 	rHead->U = 0.7;
 	ck_assert_ptr_eq(checkPeriod(&par, -99, -1), rHead->next->next);// par, prefer lower U
 
